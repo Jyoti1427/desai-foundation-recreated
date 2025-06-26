782467611c8ff13757a5526d430751488716a45d
Jyoti Singh Chandel <jyotisingh016@gmail.com>
2025-06-26 16:48:59 +0530

    Optimized bundle and fixed image issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer' // Optional, for bundle analysis
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'bundle-stats.html',
+      open: true, // Automatically opens the report after build
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // In KB, raises the warning threshold
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: {
+    //       // Split common libraries into separate chunks
+    //       vendor: ['react', 'react-dom'],
+    //       animation: ['framer-motion'],
+    //       carousel: ['react-slick', 'swiper'],
+    //     },
+    //   },
+    // },
+  },
 })
